fadffd2b93923fcb63b4cb83b3275b8eb8ae2b68
Ashwin Subramanian <abs716@nyu.edu>
2019-05-15 14:34:29 -0400

    clean and reorganize code

--- HMM/hmm.py
+++ HMM/hmm.py
@@ -1,6 +1,6 @@
-from jiwer import wer
+from jiwer import wer # used to calculate word error rate
 import time
-
+import sys
 
 # data structures to implement HMM
 observations = set() # set of English words
@@ -9,18 +9,13 @@ start_probability = dict() # Start probability of French words
 transition_probability = dict() # Transition probability of French words
 emission_probability = dict() # Emission probability of French to English words
 
-
-'''
-# convert list of counts into list of probability
-def list2probabilitydict(given_list):
-    probability_dict = {}
-    given_list_length = len(given_list)
-    for item in given_list:
-        probability_dict[item] = probability_dict.get(item, 0) + 1
-    for key, value in probability_dict.items():
-        probability_dict[key] = value / given_list_length
-    return probability_dict
-'''
+# Performance metrics to evaluate HMM
+training_time = -1
+validation_time = -1
+minWER = sys.maxsize # min word error rate when validating
+maxWER = -1 # max word error rate when validating
+totalWER = 0 # used to compute avg word error rate when validating
+totalAccuracy = 0 # used to compute avg accuracy when validating
 
 
 # Viterbi decoding algorithm to translate English sentence into French sentence
@@ -42,7 +37,7 @@ def viterbi_decode(obs):
             current_probability[current_state] = max_probability
             path[current_state].append(ls)
 
-    # find the final largest probability
+    # find final largest probability
     max_probability = -1
     max_path = None
     for s in states:
@@ -53,31 +48,37 @@ def viterbi_decode(obs):
     return max_path
 
 
-training_time = -1
-validation_time = -1
-
 # Train HMM using data from English and French input text files
 def train():
 
+    print('Training start.')
+
     start_time = time.time()
 
+    # open French text file
     fo = open('small_vocab_fr.txt', encoding='utf8')
 
+    # open English text file and iterate line by line
     for enLine in open('small_vocab_en.txt', encoding='utf8'):
 
+        # read French line
         frLine = fo.readline()
 
+        # split English and French sentence into array of words
         enTokens = enLine.strip().split(' ')
         enTokensLength = len(enTokens)
         frTokens = frLine.strip().split(' ')
         frTokensLength = len(frTokens)
 
+        # Update start prob matrix
         start_probability[frTokens[0]] = start_probability.get(frTokens[0], 0) + 1
 
+        # Add English words in current line to set of observations
         for i in range(0, enTokensLength):
             if enTokens[i] != '':
                 observations.add(enTokens[i])
 
+        # Add French words in current line to set of states. Update transition and emission matrices.
         for i in range(0, frTokensLength):
             if frTokens[i] != '':
                 states.add(frTokens[i])
@@ -100,6 +101,7 @@ def train():
                     emission_probability[frTokens[i]][enTokens[i]] = emission_probability[frTokens[i]]\
                                                                          .get(enTokens[i], 0) + 1
 
+    # Fill in remaining values for start prob, transition, and emission matrices with zeros
     for state in states:
         if state not in start_probability.keys():
             start_probability[state] = 0
@@ -112,181 +114,97 @@ def train():
             if ob not in value.keys():
                 emission_probability[key][ob] = 0
 
-    '''
-    goodS = 0
-    goodT = 0
-    goodS2 = 0
-    goodT2 = 0
-    goodE = 0
-    goodE2 = 0
-
-    for k, v in start_probability.items():
-        if v > 0:
-            # print('goodS')
-            # print(v)
-            goodS += 1
-
-    for k, v in transition_probability.items():
-        for a, b in v.items():
-            if b > 0:
-                # print('goodT')
-                # print(b)
-                goodT += 1
-
-    for k, v in emission_probability.items():
-        for a, b in v.items():
-            if b > 0:
-                # print('goodT')
-                # print(b)
-                goodE += 1
-
-    # print(start_probability)
-    # print(transition_probability)
-
-    # exit(0)
-    '''
-
     # normalize start probability, transition probability, and emission probability matrices
     # into probability distributions
     start_probability_total = sum(start_probability.values())
-    for key, value in start_probability.items():
-        start_probability[key] = value / start_probability_total
+    for token, count in start_probability.items():
+        start_probability[token] = count / start_probability_total
 
     for prev_word, next_word_list in transition_probability.items():
-        s = sum(next_word_list.values())
-        if s == 0:
-            continue
-        for k, v in next_word_list.items():
-            next_word_list[k] = v / s
-        # transition_probability[prev_word] = list2probabilitydict(next_word_list)
-        transition_probability[prev_word] = next_word_list
+        row_total = sum(next_word_list.values())
+        if row_total != 0:
+            for token, count in next_word_list.items():
+                next_word_list[token] = count / row_total
+            transition_probability[prev_word] = next_word_list
 
-    for prev_word, next_word_list in emission_probability.items():
-        s = sum(next_word_list.values())
-        if s == 0:
-            continue
-        for k, v in next_word_list.items():
-            next_word_list[k] = v / s
-        # emission_probability[prev_word] = list2probabilitydict(next_word_list)
-        emission_probability[prev_word] = next_word_list
+    for word, emitted_word_list in emission_probability.items():
+        row_total = sum(emitted_word_list.values())
+        if row_total != 0:
+            for token, count in emitted_word_list.items():
+                emitted_word_list[token] = count / row_total
+            emission_probability[word] = emitted_word_list
 
-    '''
-    for k, v in start_probability.items():
-        if v > 0:
-            # print('goodS')
-            # print(v)
-            goodS2 += 1
-
-    for k, v in transition_probability.items():
-        for a, b in v.items():
-            if b > 0:
-                # print('goodT')
-                # print(b)
-                goodT2 += 1
-
-    for k, v in emission_probability.items():
-        for a, b in v.items():
-            if b > 0:
-                # print('goodT')
-                # print(b)
-                goodE2 += 1
-
-    print(goodS, ' ', goodS2, ' ', goodT, ' ', goodT2, ' ', goodE, ' ', goodE2)
-
-    assert (goodS == goodS2)
-    assert (goodT == goodT2)
-    assert (goodE == goodE2)
-
-    # print(observations)
-    # print(states)
-    # print(start_probability)
-    # print(transition_probability)
-    # print(emission_probability)
-
-    # exit(0)
-
-    # print(len(observations))
-    # print(len(states))
-    # print(len(start_probability))
-    # print(len(transition_probability))
-    # for k, v in transition_probability.items():
-    #    print('T ', len(v))
-    # print(len(emission_probability))
-    # for k, v in emission_probability.items():
-    #    print('E ', len(v))
-    '''
-
+    # compute training time
     global training_time
     training_time = time.time() - start_time
 
+    print('Training successful. Training time: ', training_time * 1000, ' ms')
 
-minWER = 1000
-maxWER = -1
-totalWER = 0
 
-def validate():
+# Validate HMM using data from first 100 lines of English and French files
+def validate(validation_size):
+
+    print('Validation start.')
+
     start_time = time.time()
 
-    ct = 0
+    lineNum = 1
     validation_acc_sum = 0
 
+    # Open French file. Start from beginning.
     fo = open('small_vocab_fr.txt', encoding='utf8')
+
+    # Open English file at beginning. Iterate line by line.
     for enLine in open('small_vocab_en.txt', encoding='utf8'):
 
-        print(ct)
-
-        #if ct < training_size:
-        #    ct += 1
-        #    continue
-
-        if ct == 100:
+        # Stop after validating the specified number of lines
+        if lineNum == validation_size + 1:
             break
 
-        #print('Validation ', ct)
+        print('---------------------------------')
+        print('Validating Line ', lineNum)
 
+        # Read French line
         frLine = fo.readline()
+
+        # Split English and French sentence into tokens
         frTokens = frLine.strip().split(' ')
-        print(frTokens)
         enTokens = enLine.strip().split(' ')
-        #ans = viterbi_decode(enTokens, states, start_probability, transition_probability, emission_probability)
-        ans = viterbi_decode(enTokens)
-        #print(ans)
-        #if ct == training_size:
-        #    print(frTokens)
-        #    print(ans)
-        werx = wer(frTokens, ans)
 
-        global minWER, maxWER, totalWER, validation_time
+        # Translate English to French sentence using HMM and Viterbi decoding
+        viterbi_result = viterbi_decode(enTokens)
 
-        if werx < minWER:
-            minWER = werx
+        # compute word error rate. reference = frTokens, hypothesis = viterbi_result
+        wer_value = wer(frTokens, viterbi_result)
 
-        if werx > maxWER:
-            maxWER = werx
+        global minWER, maxWER, totalWER, totalAccuracy
 
-        totalWER = totalWER + werx
+        # find min, max, and total WER so far
+        if wer_value < minWER:
+            minWER = wer_value
 
-        print(werx)
-        if werx < 1.0:
-            print(frTokens)
-            print(ans)
-            #print(werx - len(frTokens))
-            #print(werx - len(ans))
-        print('----------')
-            #wacc = 1 - werx
-            #if wacc < 0:
-            #    wacc = 0
-            #print(wer)
-            #validation_acc_sum += wacc
+        if wer_value > maxWER:
+            maxWER = wer_value
 
-        ct += 1
+        totalWER += wer_value
 
-    #validation_accuracy = validation_acc_sum / ct
+        # compute translation accuracy of current line based on WER. accuracy >= 0.
+        accuracy = max(1 - wer_value, 0)
 
+        totalAccuracy += accuracy
+
+        print('Desired Result: ', ' '.join(frTokens))
+        print('Actual Result : ', ' '.join(viterbi_result))
+        print('Word Error Rate: ', wer_value)
+        print('Translation Accuracy: ', accuracy * 100, ' %')
+
+        lineNum += 1
+
+    # compute validation time
+    global validation_time
     validation_time = time.time() - start_time
 
-    #print(validation_accuracy)
-
+    print('Validation complete.')
 
 if __name__ == '__main__':
 
@@ -294,11 +212,16 @@ if __name__ == '__main__':
     train()
 
     # Validate HMM with data from first 100 lines of each file
-    validate()
+    validation_size = 100
+    validate(validation_size)
 
     # Print summary
+    print('---------------------------------')
+    print('Summary')
+    print()
     print('Training Time: ', training_time * 1000, ' ms')
     print('Validation Time: ', validation_time * 1000, ' ms')
     print('Min WER: ', minWER)
     print('Max WER: ', maxWER)
-    print('Average WER: ', totalWER / 100)
+    print('Average WER: ', totalWER / validation_size)
+    print('Average Translation Accuracy: ', totalAccuracy / validation_size * 100, ' %')
